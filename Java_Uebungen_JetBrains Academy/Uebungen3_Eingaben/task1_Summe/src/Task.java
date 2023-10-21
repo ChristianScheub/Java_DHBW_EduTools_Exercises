91a7249960bad4c544dfb4dcfd12e52f7a1f4541
Christian Scheub <christian.developer.app@gmail.com>
2023-10-21 12:10:50 +0200

    Fix

--- Java_Uebungen_JetBrains Academy/Uebungen3_Eingaben/task1_Summe/src/Task.java
+++ Java_Uebungen_JetBrains Academy/Uebungen3_Eingaben/task1_Summe/src/Task.java
@@ -12,9 +12,7 @@ public class Task {
 
     public static int addition(int n) {
         int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
+        
         return sum;
     }
 }
