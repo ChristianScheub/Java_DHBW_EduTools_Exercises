da09b35adbecc2183c912405117a41743e9b124b
Christian Scheub <christian.developer.app@gmail.com>
2023-10-22 16:32:04 +0200

    Fixes in Blatt 3,5
    Teilweise Lösungen drinnen gelassen und nun entfernt xD

--- Java_Uebungen_JetBrains Academy/Uebungen3_Eingaben/task1_Summe/src/Task.java
+++ Java_Uebungen_JetBrains Academy/Uebungen3_Eingaben/task1_Summe/src/Task.java
@@ -5,19 +5,13 @@ public class Task {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            int n = scanner.nextInt();
-            int sum = addition(n);
-            System.out.print(sum);
+
         } catch (NumberFormatException e) {
             System.out.println("Ungültige Eingabe. Bitte geben Sie eine ganze Zahl als Argument ein.");
         }
     }
 
     public static int addition(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+
     }
 }
--- Java_Uebungen_JetBrains Academy/Uebungen3_Eingaben/task3_Tabelle/src/Task.java
+++ Java_Uebungen_JetBrains Academy/Uebungen3_Eingaben/task3_Tabelle/src/Task.java
@@ -4,20 +4,6 @@ public class Task {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Bitte geben Sie eine Zahl für die Multiplikationstabelle ein: ");
-        int n;
 
-        try {
-            n = scanner.nextInt();
-        } catch (Exception e) {
-            System.out.println("Das eingegebene Argument ist keine gültige Zahl.");
-            scanner.close();
-            return;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            System.out.printf("%d x %d = %d%n", i, n, i * n);
-        }
-        scanner.close();
     }
 }
