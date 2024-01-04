bd5d5e0c50eb7692c416bc92b50bd3ec99369b8b
Bernhard Volz <bernhard.volz@hs-ansbach.de>
2024-01-04 11:25:43 +0100

    2.5: Aufgabe zur Modulo-Operation

---
+++ HSAN_Programmierung_1/Lektion 02 - Datentypen und Tastatureingaben/05 - Modulo/src/Modulo.java
@@ -0,0 +1,11 @@
+public class Modulo {
+    public static void main(String[] args) {
+        double pi = 3.1415;
+
+        double nk = pi % 1;
+        double vk = pi - nk;
+
+        System.out.println(nk);
+        System.out.println(vk);
+    }
+}
