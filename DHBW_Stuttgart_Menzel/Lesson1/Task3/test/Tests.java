3de97ac94817ed38e9f7cfeca2ba4c8d9e2c4590
Masl321 <marcelsteffen414@gmail.com>
2024-05-01 18:25:01 +0200

    1.3 Tests

--- DHBW_Stuttgart_Menzel/Lesson1/Task3/test/Tests.java
+++ DHBW_Stuttgart_Menzel/Lesson1/Task3/test/Tests.java
@@ -5,55 +5,44 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class Tests {
-  @Test
-  public void testSolution() {
-    Class<?> arrShift = null;
-    try {
-      arrShift = Class.forName("ArrayShifter");
-    } catch (ClassNotFoundException e) {
-      Assert.fail("Klasse wurde umbenannt/gelöscht");
-      return;
-    }
-    Constructor<?> constructor = null;
-    try {
-      constructor = arrShift.getConstructor();
-    } catch (NoSuchMethodException e) {
-      Assert.fail("Klasse weist strukturelle Fehler auf");
-      return;
-    }
-    Method generateArray = null;
-    try {
-      generateArray = arrShift.getDeclaredMethod("generateRandomArray", int.class, int.class, int.class);
-    } catch (NoSuchMethodException e) {
-      Assert.fail("Methode generate Array nicht Implementiert");
-      return;
-    }
-    Object o = null;
-    try {
-      o = constructor.newInstance();
-    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-      Assert.fail("Klasse hat einen strukturellen Fehler oder konnte nicht erfolgreich ausführen");
-      e.getCause().printStackTrace();
-    }
-    try {
-      int[] result = (int[]) generateArray.invoke(o,2,1,2);
-      Assert.assertEquals(2,result.length);
-      List<int[]> testArrs = new ArrayList<>();
-      testArrs.add(new int[]{1,1});
-      testArrs.add(new int[]{1,2});
-      testArrs.add(new int[]{2,1});
-      testArrs.add(new int[]{2,2});
-      boolean found = false;
-      for (int[] x:testArrs) {
-        if(Arrays.equals(x,result)){
-          found = true;
+    @Test
+    public void testSolution() {
+        Class<?> arrShift = null;
+        try {
+            arrShift = Class.forName("ArrayShifter");
+        } catch (ClassNotFoundException e) {
+            Assert.fail("Klasse wurde umbenannt/gelöscht");
+            return;
         }
-      }
-      if(!found){
+        Constructor<?> constructor = null;
+        try {
+            constructor = arrShift.getConstructor();
+        } catch (NoSuchMethodException e) {
+            Assert.fail("Klasse weist strukturelle Fehler auf");
+            return;
+        }
+        Method shiftarr = null;
+        try {
+            shiftarr = arrShift.getDeclaredMethod("shiftArray", int[].class, int.class);
+        } catch (NoSuchMethodException e) {
+            Assert.fail("Methode generate Array nicht Implementiert");
+            return;
+        }
+        Object o = null;
+        try {
+            o = constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            Assert.fail("Klasse hat einen strukturellen Fehler oder konnte nicht erfolgreich ausführen");
+            e.getCause().printStackTrace();
+        }
+        try {
+            int[] result = (int[]) shiftarr.invoke(o, new int[]{3, 2, 1, 1}, 2);
+            Assert.assertArrayEquals(new int[]{1, 1, 3, 2}, result);
+            result = (int[]) shiftarr.invoke(o,new int[]{1,2,3},5);
+            Assert.assertArrayEquals(new int[]{2,3,1},result);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
 
-      }
-    } catch (IllegalAccessException | InvocationTargetException e) {
-      throw new RuntimeException(e);
     }
-  }
 }
